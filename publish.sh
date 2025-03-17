2e887cd3f58177ba457acc298d8b2db97ddaa6e5
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-03-17 15:59:42 +0100

    Prevent batch not building any repos when 1 cannot be cloned (#29)
    * Prevent batch not building any repos when 1 cannot be cloned

--- publish.sh
+++ publish.sh
@@ -55,11 +55,7 @@ function build_and_upload_repos() {
     # extract just the partition name from the file name
     partition_name=$(echo "$file" | cut -d'-' -f2)
 
-    # if cloning failed, skip the rest of the loop
-    if ! mod git clone csv "$partition_name" "$file" --filter=tree:0; then
-      printf "[%d][%s] Cloning failed, skipping partition\n" $index "$partition_name"
-      continue
-    fi
+    mod git clone csv "$partition_name" "$file" --filter=tree:0
 
     mod build "./$partition_name" --no-download
 
